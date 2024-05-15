f17ed3a2adb4f39ec60775e2b1e9e4f2d72b966b
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-15 21:51:13 +0100

    Add login and signup component

--- Bank-service-front/src/app/app.module.ts
+++ Bank-service-front/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddNewClientComponent } from './add-new-client/add-new-client.component';
+import { NewBankAccountComponent } from './new-bank-account/new-bank-account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewPaymentComponent } from './new-payment/new-payment.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    AddNewClientComponent,
+    NewBankAccountComponent,
+    DashboardComponent,
+    NewPaymentComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
