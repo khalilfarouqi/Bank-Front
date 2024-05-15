826b25792e790e9c2cf28d18f60470533802c18b
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-15 21:51:35 +0100

    add others component

---
+++ Bank-service-front/src/app/new-bank-account/new-bank-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-bank-account',
+  templateUrl: './new-bank-account.component.html',
+  styleUrl: './new-bank-account.component.css'
+})
+export class NewBankAccountComponent {
+
+}
