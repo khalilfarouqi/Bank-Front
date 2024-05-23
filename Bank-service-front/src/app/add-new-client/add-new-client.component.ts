ffac7b56987c02b5522ab13ed1eae41aa1d0c3a2
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-23 22:52:49 +0100

    add loading effect

--- Bank-service-front/src/app/add-new-client/add-new-client.component.ts
+++ Bank-service-front/src/app/add-new-client/add-new-client.component.ts
@@ -12,10 +12,16 @@ export class AddNewClientComponent {
 
   @ViewChild('clientForm') clientForm!: NgForm;
   errorMessage: any;
+  isLoading = false;
+
   constructor(private customerService: CustomerService) {}
 
   addCustomer(formData: any) {
+    this.isLoading = true;
+    this.errorMessage = null;
+
     console.log(formData);
+
     if (formData.dateOfBirth) {
       const date = new Date(formData.dateOfBirth);
       const year = date.getFullYear();
@@ -23,7 +29,9 @@ export class AddNewClientComponent {
       const day = ('0' + date.getDate()).slice(-2);
       formData.dateOfBirth = `${year}/${month}/${day}`;
     }
+
     formData.username = `${formData.firstName}.${formData.lastName}`;
+
     this.customerService.createCustomer(formData).subscribe({
       next: (response) => {
         console.log('Customer created', response);
@@ -32,6 +40,9 @@ export class AddNewClientComponent {
       error: (error) => {
         console.error('GraphQL Error', error);
         this.handleError(error);
+      },
+      complete: () => {
+        this.isLoading = false;
       }
     });
   }
