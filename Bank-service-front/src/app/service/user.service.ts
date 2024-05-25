337334d90213e24ca2a022f29e4f78d4287c0f40
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-25 23:13:11 +0100

    add changement de password

--- Bank-service-front/src/app/service/user.service.ts
+++ Bank-service-front/src/app/service/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const UPDATE_PASSWORD = gql`
+  mutation updatePassword($dto: UpdatePasswordRequest!) {
+    updatePassword(dto: $dto) {
+      message
+    }
+  }
+`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  updatePassword(dto: any) {
+    return this.apollo.mutate({
+      mutation: UPDATE_PASSWORD,
+      variables: {
+        dto,
+      },
+    });
+  }
 }
