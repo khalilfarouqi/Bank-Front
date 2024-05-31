460f4a5b63043ea1e9ac217b75cf2293763e1168
khalilfarouqi <bio.khalil.f@gmail.com>
2024-05-31 13:02:42 +0100

    correct role

--- Bank-service-front/src/app/guard/auth.guard.spec.ts
+++ Bank-service-front/src/app/guard/auth.guard.spec.ts
@@ -1,17 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { AuthGuard } from './auth.guard';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let authGuard: AuthGuard;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
+
+    authGuard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(authGuard).toBeTruthy();
+  });
+
+  it('should return true for a logged in user', () => {
+    authService.isAuthenticated.and.returnValue(true);
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+    expect(authGuard.canActivate(route, state)).toBe(true);
+    expect(authService.isAuthenticated).toHaveBeenCalled();
+  });
+
+  it('should navigate to login for a logged out user', () => {
+    authService.isAuthenticated.and.returnValue(false);
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+    expect(authGuard.canActivate(route, state)).toBe(false);
+    expect(authService.isAuthenticated).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
